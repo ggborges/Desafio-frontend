da327757f5ecfe72f6ba1f6591980f8e8e602933
Gustavo Borges <ggb@cin.ufpe.br>
2022-11-04 05:14:07 -0300

    signup-page component criado e ajustes nas rotas para signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: '', component: LoginPageComponent  }
+  { path: '', component: LoginPageComponent  },
+  { path: 'signup', component: SignupPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
