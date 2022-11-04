743b291f5937a5a4c783bb2619be798aab347bcc
Gustavo Borges <ggb@cin.ufpe.br>
2022-11-04 02:49:27 -0300

    login-page component adicionado e rotas na nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
